7bd3f3beeebd8d3d66219a692a51a3584e9f1d16
Ronald Huereca <rhuereca@bigwing.com>
2016-08-23 10:50:08 -0500

    Re-factor main plugin file.

--- gravity-forms-event-tracking.php
+++ gravity-forms-event-tracking.php
@@ -25,10 +25,73 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-class Gravity_Forms_Event_Tracking_Bootstrap {
-
-	public static function load(){
-		if ( ! gravity_forms_event_tracking_has_minimum_php() ) return;
+class GFGAET {
+	
+	/**
+	 * Holds the class instance.
+	 *
+	 * @since 2.0.0
+	 * @access private
+	 */
+	private static $instance = null;
+	
+	/**
+	 * Retrieve a class instance.
+	 *
+	 * @since 2.0.0
+	 */
+	public static function get_instance() {
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	} //end get_instance
+	
+	/**
+	 * Retrieve the plugin basename.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @return string plugin basename
+	 */
+	public static function get_plugin_basename() {
+		return plugin_basename( __FILE__ );	
+	}
+	
+	/**
+	 * Class constructor.
+	 *
+	 * @since 2.0.0
+	 */
+	private function __construct() {
+		load_plugin_textdomain( 'gravity-forms-google-analytics-event-tracking', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		
+		spl_autoload_register( array( $this, 'loader' ) );
+		
+		add_action( 'gform_loaded', array( $this, 'gforms_loaded' ) );
+	}
+	
+	/**
+	 * Check for the minimum supported PHP version.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @return bool true if meets minimum version, false if not
+	 */
+	public static function check_php_version() {
+		if( ! version_compare( '5.3', PHP_VERSION, '<=' ) ) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Initialize Gravity Forms related add-ons.
+	 *
+	 * @since 2.0.0
+	 */
+	public function gforms_loaded() {
+		if ( ! GFGAET::check_php_version() ) return;
 		if ( ! method_exists( 'GFForms', 'include_feed_addon_framework' ) ) {
 			return;
 		}
@@ -36,29 +99,53 @@ class Gravity_Forms_Event_Tracking_Bootstrap {
 		require_once( plugin_dir_path( __FILE__ ) . 'includes/class-gravity-forms-event-tracking-feed.php' );
 
 		GFAddOn::register( 'Gravity_Forms_Event_Tracking' );
-
 	}
-
+	
+	/**
+	 * Autoload class files.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param string $class_name The class name
+	 */
+	public function loader( $class_name ) {
+		if ( class_exists( $class_name, false ) || false === strpos( $class_name, 'GFGAET' ) ) {
+			return;
+		}
+		$file = GFGAET::get_plugin_dir( "includes/{$class_name}.php" );
+		if ( file_exists( $file ) ) {
+			include_once( $file );
+		}
+	}
+	
+	/**
+	 * Return the absolute path to an asset.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param string @path Relative path to the asset.
+	 *
+	 * return string Absolute path to the relative asset.
+	 */
+	public static function get_plugin_dir( $path = '' ) {
+		$dir = rtrim( plugin_dir_path(__FILE__), '/' );
+		if ( !empty( $path ) && is_string( $path) )
+			$dir .= '/' . ltrim( $path, '/' );
+		return $dir;		
+	}
+	
+	/**
+	 * Check the plugin to make sure it meets the minimum requirements.
+	 *
+	 * @since 2.0.0
+	 */
+	public function check_plugin() {
+		if( ! GFGAET::check_php_version() ) {
+			deactivate_plugins( GFGAET::get_plugin_basename() );
+			exit( sprintf( esc_html__( 'Gravity Forms Event Tracking requires PHP version 5.3 and up. You are currently running PHP version %s.', 'gravity-forms-google-analytics-event-tracking' ), esc_html( PHP_VERSION ) ) );
+		}
+	}
 }
 
-add_action( 'gform_loaded', array( 'Gravity_Forms_Event_Tracking_Bootstrap', 'load' ), 5 );
-
-
-
-//Make sure minimum version of PHP version is supported
-//Returns true if minimum PHP is met, false if not
-function gravity_forms_event_tracking_has_minimum_php() {
-	if( ! version_compare( '5.3', PHP_VERSION, '<=' ) ) {
-		return false;
-	}
-	return true;
-}
-
-//Spit out error if user isn't using minimum version of PHP
-function gravity_forms_event_tracking_activation() {
-	if( !gravity_forms_event_tracking_has_minimum_php() ) {
-		deactivate_plugins( plugin_basename( __FILE__ ) );
-		exit( sprintf( esc_html__( 'Gravity Forms Event Tracking requires PHP version 5.3 and up. You are currently running PHP version %s.', 'gravity-forms-google-analytics-event-tracking' ), esc_html( PHP_VERSION ) ) );
-	}
-}
-register_activation_hook( __FILE__, 'gravity_forms_event_tracking_activation' );
+register_activation_hook( __FILE__, array( 'GFGAET', 'check_plugin' ) );
+GFGAET::get_instance();
