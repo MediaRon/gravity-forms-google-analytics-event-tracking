ed6729a31b85888a57328feba660e78b26d56f55
Ronald Huereca <rhuereca@updraftplus.com>
2018-10-02 22:10:59 -0500

    Saving GF Entries.

--- includes/GFGAET_Partial_Entries.php
+++ includes/GFGAET_Partial_Entries.php
@@ -18,6 +18,7 @@ class GFGAET_Partial_Entries extends GFAddOn {
 	public function init_admin() {
 		parent::init_admin();
 		add_action( 'gform_field_advanced_settings', array( $this, 'advanced_settings' ), 10, 2 );
+		add_action( 'gform_editor_js', array( $this, 'editor_script' ) );
 	}
 	public function minimum_requirements() {
 		return array(
@@ -27,29 +28,48 @@ class GFGAET_Partial_Entries extends GFAddOn {
 			),
 		);
 	}
+	public function editor_script() {
+		/*
+		 * Add .field_id_setting onto the end of each field
+		 * type's properties.
+		 */
+		?>
+		<script type="text/javascript">
+		jQuery.map(fieldSettings, function (el, i) {
+			fieldSettings[i] += ', .admin_partial_entry_category, .admin_partial_entry_action, .admin_partial_entry_label';
+		});
+		// Populate field settings on initialization.
+		jQuery(document).on('gform_load_field_settings', function(ev, field){
+			jQuery(document.getElementById('field_partial_entries_category')).val(field.field_partial_entries_category || '');
+			jQuery(document.getElementById('field_partial_entries_action')).val(field.field_partial_entries_action || '');
+			jQuery(document.getElementById('field_partial_entries_label')).val(field.field_partial_entries_label || '');
+		});
+		</script>
+		<?php
+	}
 	public function advanced_settings( $position, $form_id ) {
 		if( 100 !== $position ) return;
 		?>
-		<li class="admin_label_setting admin_partial_entry_category field_setting">
+		<li class="admin_partial_entry_category field_setting">
             <label for="field_partial_entries_category" class="section_label">
                 <?php _e( 'Event Category', 'gravity-forms-google-analytics-event-tracking' ); ?>
                 <?php gform_tooltip( 'gfgaet_category' ); ?>
             </label>
-            <input type="text" id="field_partial_entries_category" class="partial-entries-category fieldwidth-2 merge-tag-support mt-position-right mt-prepopulate" onChange="SetFieldProperty( 'field_partial_entries_category', this.value );" />
+            <input type="text" id="field_partial_entries_category" class="partial-entries-category fieldwidth-2 merge-tag-support mt-position-right mt-prepopulate" onChange="SetFieldProperty( 'field_partial_entries_category', this.value );" oninput="SetFieldProperty( 'field_partial_entries_label', this.value );" />
 		</li>
-		<li class="admin_label_setting admin_partial_entry_action field_setting">
-            <label for="field_partial_entrie_action" class="section_label">
+		<li class="admin_partial_entry_action field_setting">
+            <label for="field_partial_entries_action" class="section_label">
                 <?php _e( 'Event Action', 'gravity-forms-google-analytics-event-tracking' ); ?>
                 <?php gform_tooltip( 'gfgaet_action' ); ?>
             </label>
-            <input type="text" id="field_partial_entrie_action" class="partial-entries-action fieldwidth-2 merge-tag-support mt-position-right mt-prepopulate" onChange="SetFieldProperty( 'field_partial_entrie_action', this.value );" />
+            <input type="text" id="field_partial_entries_action" class="partial-entries-action fieldwidth-2 merge-tag-support mt-position-right mt-prepopulate" onChange="SetFieldProperty( 'field_partial_entries_action', this.value );" oninput="SetFieldProperty( 'field_partial_entries_label', this.value );" />
 		</li>
-		<li class="admin_label_setting admin_partial_entry_label field_setting">
+		<li class="admin_partial_entry_label field_setting">
             <label for="field_partial_entries_label" class="section_label">
                 <?php _e( 'Event Label', 'gravity-forms-google-analytics-event-tracking' ); ?>
                 <?php gform_tooltip( 'gfgaet_label' ); ?>
             </label>
-            <input type="text" id="field_partial_entries_label" class="partial-entries-label fieldwidth-2 merge-tag-support mt-position-right mt-prepopulate" onChange="SetFieldProperty( 'field_partial_entries_label', this.value );" />
+            <input type="text" id="field_partial_entries_label" class="partial-entries-label fieldwidth-2 merge-tag-support mt-position-right mt-prepopulate" onChange="SetFieldProperty( 'field_partial_entries_label', this.value );" oninput="SetFieldProperty( 'field_partial_entries_label', this.value );" />
         </li>
 		<?php
 	}
