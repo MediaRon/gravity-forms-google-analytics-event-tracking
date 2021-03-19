9647ce861e435661e9fc49f0ab4de33839e4c5ff
Ronald Huereca <ronalfy@gmail.com>
2021-03-19 10:59:12 -0500

    Bumping version and code formatting.

--- includes/GFGAET_Pagination.php
+++ includes/GFGAET_Pagination.php
@@ -15,7 +15,7 @@ class GFGAET_Pagination {
 	 */
 	public static function get_instance() {
 		if ( null == self::$instance ) {
-			self::$instance = new self;
+			self::$instance = new self();
 		}
 		return self::$instance;
 	} //end get_instance
@@ -34,9 +34,9 @@ class GFGAET_Pagination {
 	 *
 	 * @since 2.0.0
 	 *
-	 * @param array $form                The form arguments
+	 * @param array                                               $form                The form arguments
 	 * @param int   @source_page_number  The original page number
-	 * @param int   $current_page_number The new page number
+	 * @param int                                                 $current_page_number The new page number
 	 */
 	public function paginate( $form, $source_page_number, $current_page_number ) {
 
@@ -60,7 +60,7 @@ class GFGAET_Pagination {
 			$event_category = 'form';
 			if ( isset( $form['pagination_category'] ) ) {
 				$pagination_category = trim( $form['pagination_category'] );
-				if( ! empty( $pagination_category ) ) {
+				if ( ! empty( $pagination_category ) ) {
 					$event_category = $pagination_category;
 				}
 			}
@@ -81,7 +81,7 @@ class GFGAET_Pagination {
 			$event_action = 'pagination';
 			if ( isset( $form['pagination_action'] ) ) {
 				$pagination_action = trim( $form['pagination_action'] );
-				if( ! empty( $pagination_action ) ) {
+				if ( ! empty( $pagination_action ) ) {
 					$event_action = $pagination_action;
 				}
 			}
@@ -102,11 +102,11 @@ class GFGAET_Pagination {
 			$event_label = sprintf( '%s::%d::%d', esc_html( $form['title'] ), absint( $source_page_number ), absint( $current_page_number ) );
 			if ( isset( $form['pagination_label'] ) ) {
 				$pagination_label = trim( $form['pagination_label'] );
-				if( ! empty( $pagination_label ) ) {
+				if ( ! empty( $pagination_label ) ) {
 					$pagination_label = str_replace( '{form_title}', esc_html( $form['title'] ), $pagination_label );
 					$pagination_label = str_replace( '{source_page_number}', absint( $source_page_number ), $pagination_label );
 					$pagination_label = str_replace( '{current_page_number}', absint( $current_page_number ), $pagination_label );
-					$event_label = $pagination_label;
+					$event_label      = $pagination_label;
 				}
 			}
 			$event_label = apply_filters( 'gform_pagination_event_label', $event_label, $form, $source_page_number, $current_page_number );
@@ -126,12 +126,12 @@ class GFGAET_Pagination {
 			$event_value = 0;
 			if ( isset( $form['pagination_value'] ) ) {
 				$pagination_value = trim( $form['pagination_value'] );
-				if( ! empty( $pagination_value ) ) {
+				if ( ! empty( $pagination_value ) ) {
 					$event_value = $pagination_value;
 				}
 			}
 			// Value is rounded up (Google likes integers only) before given an absolute value
-			$event_value = absint( round( GFCommon::to_number( apply_filters( 'gform_pagination_event_value', $event_value, $form, $source_page_number, $current_page_number  ) ) ) );
+			$event_value = absint( round( GFCommon::to_number( apply_filters( 'gform_pagination_event_value', $event_value, $form, $source_page_number, $current_page_number ) ) ) );
 
 			// Set environmental variables for the measurement protocol
 			$event->set_event_category( $event_category );
@@ -163,17 +163,22 @@ class GFGAET_Pagination {
 					} else {
 						// UA code doesn't match, use another tracker
 						window.parent.ga( 'create', '<?php echo esc_js( $ua_code ); ?>', 'auto', 'GTGAET_Tracker' );
-						window.parent.ga( 'GTGAET_Tracker.send', 'event', '<?php echo esc_js( $event_category );?>', '<?php echo esc_js( $event_action ); ?>', '<?php echo esc_js( $event_label ); ?>'<?php if ( 0 !== $event_value ) { echo ',' . "'" . esc_js( $event_value ) . "'"; } ?>);
+						window.parent.ga( 'GTGAET_Tracker.send', 'event', '<?php echo esc_js( $event_category ); ?>', '<?php echo esc_js( $event_action ); ?>', '<?php echo esc_js( $event_label ); ?>'
+																					  <?php
+																						if ( 0 !== $event_value ) {
+																							echo ',' . "'" . esc_js( $event_value ) . "'"; }
+																						?>
+						);
 					}
 				}
 				</script>
 				<?php
 				return;
-			} else if ( GFGAET::is_gtm_only() ) {
+			} elseif ( GFGAET::is_gtm_only() ) {
 				?>
 				<script>
 				if ( typeof( window.parent.dataLayer ) != 'undefined' ) {
-			    	window.parent.dataLayer.push({'event': 'GFTrackEvent',
+					window.parent.dataLayer.push({'event': 'GFTrackEvent',
 						'GFTrackCategory':'<?php echo esc_js( $event_category ); ?>',
 						'GFTrackAction':'<?php echo esc_js( $event_action ); ?>',
 						'GFTrackLabel':'<?php echo esc_js( $event_label ); ?>',
@@ -196,9 +201,9 @@ class GFGAET_Pagination {
 	 *
 	 * @since 2.0.0
 	 *
-	 * @param array $form                The form arguments
+	 * @param array                                               $form                The form arguments
 	 * @param int   @source_page_number  The original page number
-	 * @param int   $current_page_number The new page number
+	 * @param int                                                 $current_page_number The new page number
 	 */
 	public function matomo_paginate( $form, $source_page_number, $current_page_number ) {
 
@@ -221,7 +226,7 @@ class GFGAET_Pagination {
 			$event_category = 'form';
 			if ( isset( $form['pagination_category'] ) ) {
 				$pagination_category = trim( $form['pagination_category'] );
-				if( ! empty( $pagination_category ) ) {
+				if ( ! empty( $pagination_category ) ) {
 					$event_category = $pagination_category;
 				}
 			}
@@ -242,7 +247,7 @@ class GFGAET_Pagination {
 			$event_action = 'pagination';
 			if ( isset( $form['pagination_action'] ) ) {
 				$pagination_action = trim( $form['pagination_action'] );
-				if( ! empty( $pagination_action ) ) {
+				if ( ! empty( $pagination_action ) ) {
 					$event_action = $pagination_action;
 				}
 			}
@@ -263,11 +268,11 @@ class GFGAET_Pagination {
 			$event_label = sprintf( '%s::%d::%d', esc_html( $form['title'] ), absint( $source_page_number ), absint( $current_page_number ) );
 			if ( isset( $form['pagination_label'] ) ) {
 				$pagination_label = trim( $form['pagination_label'] );
-				if( ! empty( $pagination_label ) ) {
+				if ( ! empty( $pagination_label ) ) {
 					$pagination_label = str_replace( '{form_title}', esc_html( $form['title'] ), $pagination_label );
 					$pagination_label = str_replace( '{source_page_number}', absint( $source_page_number ), $pagination_label );
 					$pagination_label = str_replace( '{current_page_number}', absint( $current_page_number ), $pagination_label );
-					$event_label = $pagination_label;
+					$event_label      = $pagination_label;
 				}
 			}
 			$event_label = apply_filters( 'gform_pagination_event_label', $event_label, $form, $source_page_number, $current_page_number );
@@ -287,7 +292,7 @@ class GFGAET_Pagination {
 			$event_value = 0;
 			if ( isset( $form['pagination_value'] ) ) {
 				$pagination_value = trim( $form['pagination_value'] );
-				if( ! empty( $pagination_value ) ) {
+				if ( ! empty( $pagination_value ) ) {
 					$event_value = $pagination_value;
 				}
 			}
@@ -303,7 +308,12 @@ class GFGAET_Pagination {
 				?>
 				<script>
 				if ( typeof window.parent._paq != 'undefined' ) {
-					window.parent._paq.push(['trackEvent', '<?php echo esc_js( $event_category ); ?>', '<?php echo esc_js( $event_action ); ?>', '<?php echo esc_js( $event_label ); ?>'<?php if ( 0 !== $event_value ) { echo ',' . "'" . esc_js( $event_value ) . "'"; } ?>]);
+					window.parent._paq.push(['trackEvent', '<?php echo esc_js( $event_category ); ?>', '<?php echo esc_js( $event_action ); ?>', '<?php echo esc_js( $event_label ); ?>'
+																	   <?php
+																		if ( 0 !== $event_value ) {
+																			echo ',' . "'" . esc_js( $event_value ) . "'"; }
+																		?>
+					]);
 
 				}
 				</script>
